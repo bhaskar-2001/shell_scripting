01163e9ec9d5e8f6afe12cecaeb42324eef1df5c
Ubuntu <ubuntu@ip-172-31-42-89.ap-south-1.compute.internal>
2026-06-19 16:13:14 +0000

    Adding the very 1st file

---
+++ hello_cloud.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Hello To everyone...! Nice to meet you"
+
+echo "Hii there....!"
+
+echo "Show me how to create a folder"
+
+mkdir -p folder
+
+echo "folder is created"
+
+echo "append text to the folder "
+
+echo "This is new emp from Boston" > users.txt
+
